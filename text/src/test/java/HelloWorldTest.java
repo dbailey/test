d91f17973e7a24514403cbab5139b0f119fc92fe
Dakota Bailey <dakota@sheerid.com>
2015-12-18 22:30:59 -0700

    Initial project code

---
+++ text/src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testSomething() {
+        assertEquals(1, 1);
+    }
+
+}
